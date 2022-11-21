5393fc80d007729fa2c4d74f1f4c3faab4cabc5f
Varghese T Mascreen <varghese.t.mascreen20@gmail.com>
2022-11-21 10:36:19 +0530

    Initial Commit

---
+++ SampleFirebaseProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleFirebaseProj
+//
+//  Created by DDUKK7 on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
